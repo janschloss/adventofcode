aab6a4678f3c46897bfd212b54fd55ff05f471c4
Jan <schloss.jan@gmail.com>
2024-02-07 20:11:38 +0100

    Solved Day 1 - Part 2

--- 2023/1/main.go
+++ 2023/1/main.go
@@ -5,43 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"unicode"
+	"strings"
 )
-
-func processLine(line string) (int, error) {
-	// In Go it is common to define multiple variables at once
-	var firstDigit, lastDigit int
-
-	// Here we are iterating through each character of the line
-	for _, char := range line {
-		if unicode.IsNumber(char) {
-			// In Go, we can convert a rune to an int by subtracting the rune '0'
-			intChar := int(char - '0')
-
-			if firstDigit == 0 {
-				firstDigit = intChar
-			}
-
-			lastDigit = intChar
-		} else if unicode.IsLetter(char) {
-			continue
-		} else {
-			log.Fatal("Unknown character")
-		}
-
-	}
-
-	// Here we use string concatenation to join the first and last digit
-	concatenatedDigits, err := strconv.Atoi(strconv.Itoa(firstDigit) + strconv.Itoa(lastDigit))
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return concatenatedDigits, nil
-
-}
 
 func main() {
 	file, err := os.Open("input.txt")
@@ -57,7 +22,7 @@ func main() {
 
 	sum := 0
 
-	// Iterate through each line of the file allowing us to efficiently handle each item
+	// Iterating through each line of the file
 	for scanner.Scan() {
 		lineSum, err := processLine(scanner.Text())
 
@@ -74,4 +39,62 @@ func main() {
 	}
 
 	fmt.Println(sum)
+}
+
+func processLine(line string) (int, error) {
+	// In Go it is common to define multiple variables at once
+	var firstDigit, lastDigit int
+
+	// Find first digit from the left
+	// We use line[i:] to get the substring from i to the end of the string
+	for i := 0; i < len(line); i++ {
+		if digit, ok := findDigit(line[i:]); ok {
+			firstDigit = digit
+			break
+		}
+	}
+
+	// Find last digit from the right
+	for i := len(line) - 1; i >= 0; i-- {
+		if digit, ok := findDigit(line[i:]); ok {
+			lastDigit = digit
+			break
+		}
+	}
+
+	// Here we concatenate the digits which is required by the problem
+	return firstDigit*10 + lastDigit, nil
+
+}
+
+// We define this outside of the function to avoid creating it every time the function is called
+var wordToDigit = map[string]int{
+	"zero":  0,
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+func findDigit(partialLine string) (int, bool) {
+	// Check for a digit as the first character
+	firstChar := partialLine[0]
+
+	if firstChar >= '0' && firstChar <= '9' {
+		return int(firstChar - '0'), true
+	}
+
+	// Check for word representations of digits.
+	for word, digit := range wordToDigit {
+		if strings.HasPrefix(partialLine, word) {
+			return digit, true
+		}
+	}
+
+	return 0, false
 }
--- 2023/1/main_test.go
+++ 2023/1/main_test.go
@@ -13,6 +13,14 @@ func TestProcessLine(t *testing.T) {
 		{"pqr3stu8vwx", 38},
 		{"a1b2c3d4e5f", 15},
 		{"treb7uchet", 77},
+		{"two1nine", 29},
+		{"eightwothree", 83},
+		{"abcone2threexyz", 13},
+		{"xtwone3four", 24},
+		{"4nineeightseven2", 42},
+		{"zoneight234", 14},
+		{"7pqrstsixteen", 76},
+		{"eightknfssevenfive6jnpklczrpfeightwol", 82},
 	}
 
 	for _, test := range tests {
@@ -24,7 +32,7 @@ func TestProcessLine(t *testing.T) {
 			}
 
 			if result != test.expected {
-				t.Errorf("Expected %d, but got %d for input '%s'", test.expected, result, test.input)
+				t.Errorf("Expected %d, got %d, input '%s'", test.expected, result, test.input)
 			}
 		})
 	}
